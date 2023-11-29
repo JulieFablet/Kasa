b54857cc5dd36b8f1037fe479b87a4a9321e7f24
Julie <132382540+JulieFablet@users.noreply.github.com>
2023-11-29 21:01:10 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import Navbar from '../navbar/Navbar'
 export default function Header() {
 	return (
 		<header className='header'>
-			<Link to= "/Kasa">
+			<Link to= "/Kasa/">
 			<h1>
 				<img src={Logo} alt="kasa, location d'appartements" />
 			</h1>
